13afa99b8d36aa3bb3ea96c2fbb0849579e7918a
Mohammad Moien <mnmoienian@gmail.com>
2020-07-21 09:36:53 +0430

    add navbar to app pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+import Navbar from "./components/Navbar/";
 import "./App.css";
 
 function App() {
-  return <div>Hello from App</div>;
+  return (
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/rooms/" component={Rooms} />
+        <Route path="/room/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class SingleRoom extends Component {
+  render() {
+    return <div>Hello from Single Room page</div>;
+  }
+}
+
+export default SingleRoom;
