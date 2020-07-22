f93d7f34864cc5881c0b97db67c09830cee7f631
Mohammad Moien <mnmoienian@gmail.com>
2020-07-22 11:35:08 +0430

    add services section to home page

---
+++ src/components/Title/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Title.scss";
+
+export default function Title({ title }) {
+  return (
+    <div className="section-title">
+      <h4>{title}</h4>
+      <div></div>
+    </div>
+  );
+}
